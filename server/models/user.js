c75d99069a2df81b1cd7b6bf82018f14c5f29225
Bill Taggart <taggarw@gmail.com>
2018-12-01 23:06:55 -0700

    Add POST /user/login route

--- server/models/user.js
+++ server/models/user.js
@@ -62,13 +62,31 @@ UserSchema.statics.findByToken = function (token) {
     return Promise.reject();
   }
 
-
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   });
+};
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
 };
 
 UserSchema.pre('save', function (next) {
